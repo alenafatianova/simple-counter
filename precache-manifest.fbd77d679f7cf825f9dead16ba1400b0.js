987cb57357b06f80178f100ad48bc3bd2eb051a9
Alena Fatianova <ravemybody@gmail.com>
2020-11-02 00:02:22 +0500

    Updates

---
+++ precache-manifest.fbd77d679f7cf825f9dead16ba1400b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2f4ebdaeae323647323e4a312f09a3",
+    "url": "/simple-counter/index.html"
+  },
+  {
+    "revision": "aedc4ec225310806f018",
+    "url": "/simple-counter/static/css/main.eb3e40ec.chunk.css"
+  },
+  {
+    "revision": "366061fcb1d0aa475038",
+    "url": "/simple-counter/static/js/2.09ce145f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-counter/static/js/2.09ce145f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aedc4ec225310806f018",
+    "url": "/simple-counter/static/js/main.ccaf1e48.chunk.js"
+  },
+  {
+    "revision": "d756852a9eaffcc2ac87",
+    "url": "/simple-counter/static/js/runtime-main.53843cf4.js"
+  }
+]);
